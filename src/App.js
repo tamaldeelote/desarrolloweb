a99e3957654808bfc9a7c5ce70c0c0704e8f1805
Jassiel <adamy.garnica@gmail.com>
2024-03-15 08:44:45 -0700

    Primerformulario y cosas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './felix.gif';
+import { useState } from 'react';
+import logo from './felix2.gif';
 import './App.css';
 import Primerformulario from './componentes/Primerformulario';
 
 function App() {
+  const [personaNombre, setPersona] = useState("Jassiel");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Primerformulario></Primerformulario>
+        <Primerformulario
+        personaNombre = {personaNombre} 
+        />
         
       </header>
     </div>
--- src/componentes/Primerformulario.js
+++ src/componentes/Primerformulario.js
@@ -1,12 +1,23 @@
 import {Button, Form, Col, Row } from 'react-bootstrap';
 
-function Primerformulario(){
+function Primerformulario(personaNombre){
+    function Click(){
+        console.log(personaNombre);
+    }
+
+
     return(
-        <>
-            <Form.Label> Nombre : </Form.Label>
-            <Form.Control type="text"></Form.Control>
-            <Button>Guardar</Button>
-        </>
+        <Row>
+            <Col>
+                        <Form.Label> Kuien tu eri? : </Form.Label>
+            </Col>
+            <Col>
+                        <Form.Control type= "text" value= {personaNombre.personaNombre}></Form.Control>
+            </Col>
+            <Col>
+                        <Button onClick={Click()}>Guardar</Button>
+            </Col>
+        </Row>
     )
 }
 export default Primerformulario
